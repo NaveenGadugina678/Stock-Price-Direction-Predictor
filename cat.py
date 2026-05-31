eee15e7e30eb50de39eb370b289f7027fb7f633e
[NaveenGadugina678] <[naveeng99022@gmail.com]>
2026-05-31 14:19:27 +0530

    Lessgoo

--- cat.py
+++ cat.py
@@ -3,6 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-print("Hello Naveen!")
+Ticker = "TCS.NS"
+Start = "2020-01-01"
+End = "2024-12-31"
+Output = "Stock_data_processed.csv"
+
+print(f"Downloading data for {Ticker}")
+df = yf.download(Ticker, start = Start, end = End, auto_adjust = True)
+
+if df.empty:
+    raise ValueError(f"No data returned for {Ticker}. check ticker symbol.")
+
+print(f"\nShape : {df.shape}")
+print(f"\nColumns : {df.columns}")
 
 
